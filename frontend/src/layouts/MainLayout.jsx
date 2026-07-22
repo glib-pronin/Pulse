1e74b8d3f79036b85a0a68289b89997f823da4ab
glib-pronin <0953044497gleb@gmail.com>
2026-07-22 22:26:29 +0300

    Add fonts and style to NotFound page

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 export default function MainLayout() {
     return (
         <>
-            <h1>Pulse</h1>
             <main>
                 <Outlet />
             </main>
--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -3,12 +3,14 @@ import styles from './NotFoundPage.module.css'
 
 export default function NotFoundPage() {
     return (
-        <div>
-            <h2>Not all who wander are lost, but this page is</h2>
-            <span>The link's not working or the page is gone. Go back to keep exploring.</span>
-            <Link>
-                Back
-            </Link>
-        </div>      
+        <main>
+            <div className={styles.msgContainer}>
+                <h2 className={styles.mainText}>Not all who wander are lost, but this page is</h2>
+                <span className={styles.secondaryText}>The link's not working or the page is gone. Go back to keep exploring.</span>
+                <Link to='/' className='primary-btn'>
+                    Back
+                </Link>
+            </div>      
+        </main>
     )
 }
